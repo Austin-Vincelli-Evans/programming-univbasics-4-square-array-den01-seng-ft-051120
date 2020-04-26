8a3718534cb1288bb44662d5ced64adbf530cc69
Austin Evans <unity7591@gmail.com>
2020-04-26 17:48:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   counter = 0
   new_array= []
   while counter < array.length
-    new_array.push(array[0]**2)
+    new_array.push(array[0]**)
     counter+=1
   end
   new_array
